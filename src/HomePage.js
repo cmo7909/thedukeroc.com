74b04e32a257a91bfa192c81daf4366ac536b034
cmo7909 <cmo7909@rit.edu>
2024-09-23 17:31:16 -0400

    nav bar needs some work

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import './HomePage.css';
 
 function HomePage() {
   return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-      <p>This is the main page of The Duke Event Space website.</p>
+    <div className='info-box'>
+      <p className='info-text'>This is the main page of The Duke Event Space website.</p>
     </div>
   );
 }
