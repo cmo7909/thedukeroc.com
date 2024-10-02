1b05018505f55cd7620a00d42dfb500bfc6553ff
cmo7909 <cmo7909@rit.edu>
2024-10-02 12:47:33 -0400

    All content added to website, only missing photos

--- src/HomePage.js
+++ src/HomePage.js
@@ -4,7 +4,15 @@ import './HomePage.css';
 function HomePage() {
   return (
     <div className='info-box'>
-      <p className='info-text'>This is the main page of The Duke Event Space website.</p>
+      <p className='info-text'>The Duke offers modern amenities in a unique and historic location in downtown
+      Rochester, NY. The Aesthetic is inspired by the raw music lifestyle of New York City in
+      the late 20 th century. The Duke features a custom leather fringe chandelier, bold lighting
+      and art accents throughout, and a tiled concert stage that looks out over East Main
+      Street and Parcel 5.<br /><br />
+      The Duke offers a curated guest experience enhanced by decades of hospitality and
+      event expertise from the team that owns Avvino, La Bola &amp; Frenchman Street. When
+      you book The Duke, you will be guaranteed an exclusive experience of food, wine, and
+      cocktails, while immersing yourself in a creative one-of-a-kind venue.</p>
     </div>
   );
 }
