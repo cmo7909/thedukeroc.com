ecffd2f76e40333dcfb90596163295ec25410504
cmo7909 <cmo7909@rit.edu>
2024-10-09 20:09:14 -0400

    added photographer credit

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import './Gallery.css';
-import image1 from './/Gallery/Gallery1.jpg';
-import image2 from './/Gallery/Gallery2.jpg';
-import image3 from './/Gallery/Gallery3.jpg';
-import image4 from './/Gallery/Gallery4.jpg';
-import image5 from './/Gallery/Gallery5.jpg';
-import image6 from './/Gallery/Gallery6.jpg';
-import image7 from './/Gallery/Gallery7.jpg';
-import image8 from './/Gallery/Gallery8.jpg';
-import image9 from './/Gallery/Gallery9.jpg';
-import image10 from './/Gallery/Gallery10.jpg';
-import image11 from './/Gallery/Gallery11.jpg';
-import image12 from './/Gallery/Gallery12.jpg';
-import image13 from './/Gallery/Gallery13.jpg';
-import image14 from './/Gallery/Gallery14.jpg';
-import image15 from './/Gallery/Gallery15.jpg';
-import image16 from './/Gallery/Gallery16.jpg';
-import image17 from './/Gallery/Gallery17.jpg';
-import image18 from './/Gallery/Gallery18.jpg';
-import image19 from './/Gallery/Gallery19.jpg';
-import image20 from './/Gallery/Gallery20.jpg';
+import image1 from './Gallery/Gallery1.jpg';
+import image2 from './Gallery/Gallery2.jpg';
+import image3 from './Gallery/Gallery3.jpg';
+import image4 from './Gallery/Gallery4.jpg';
+import image5 from './Gallery/Gallery5.jpg';
+import image6 from './Gallery/Gallery6.jpg';
+import image7 from './Gallery/Gallery7.jpg';
+import image8 from './Gallery/Gallery8.jpg';
+import image9 from './Gallery/Gallery9.jpg';
+import image10 from './Gallery/Gallery10.jpg';
+import image11 from './Gallery/Gallery11.jpg';
+import image12 from './Gallery/Gallery12.jpg';
+import image13 from './Gallery/Gallery13.jpg';
+import image14 from './Gallery/Gallery14.jpg';
+import image15 from './Gallery/Gallery15.jpg';
+import image16 from './Gallery/Gallery16.jpg';
+import image17 from './Gallery/Gallery17.jpg';
+import image18 from './Gallery/Gallery18.jpg';
+import image19 from './Gallery/Gallery19.jpg';
+import image20 from './Gallery/Gallery20.jpg';
 
 function GalleryImage({ src, alt }) {
   return (
@@ -49,16 +49,31 @@ function Gallery() {
     { src: image16, alt: 'Image 16' },
     { src: image19, alt: 'Image 19' },
     { src: image17, alt: 'Image 17' },
-    { src: image18, alt: 'Image 18' }, 
+    { src: image18, alt: 'Image 18' },
     { src: image20, alt: 'Image 20' },
-
   ];
 
   return (
-    <div className="gallery-container">
-      {images.map((image, index) => (
-        <GalleryImage key={index} src={image.src} alt={image.alt} />
-      ))}
+    <div className="gallery-page">
+      <div className="gallery-container">
+        {images.map((image, index) => (
+          <GalleryImage key={index} src={image.src} alt={image.alt} />
+        ))}
+      </div>
+
+      {/* Photographer Credit */}
+      <div className="photographer-credit">
+        <p>
+          Photographs by {''}
+          <a
+            href="https://johnschlia.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            [John Schlia]
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
